ecce963beae84f3e8036661bb0417bcbb3c41a3e
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2023-06-07 22:38:45 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltersComponent } from './shared/components/filters/filters.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { DataTableComponent } from './shared/components/data-table/data-table.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponent,
+    WelcomePageComponent,
+    NavbarComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule { }
